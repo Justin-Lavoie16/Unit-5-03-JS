2abef66d2061ad45d48766b26d4202ec3280d670
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-11-25 18:44:31 +0000

    423

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/Unit-5-03-JS/sw.js", {
+    scope: "/Unit-5-03-JS/",
   })
 }
 
-;("use strict")
+"use strict"
 
-function calculate() {
-  const length = parseInt(document.getElementById("pay").value)
-  const width = parseInt(document.getElementById("pay2").value)
-
-  const area = length * width * 0.18
-  const perimeter = length * width * (1.0 - 0.18)
-  const TAX_RATE = 0.18
-
-  document.getElementById("area").innerHTML =
-    "The governemnt will take: " + area + "$"
-  document.getElementById("perimeter").innerHTML =
-    "your pay will be: " + perimeter + "$"
+function myButtonClicked() {
+ 
+  const age = document.getElementById("age").value;
+  
+  if(age >= 17) {
+     document.getElementById("answers").innerHTML =
+      ("You can see an R movie alone.");
+    
+} else if(age >= 13) {
+    document.getElementById("answers").innerHTML =
+      ("You can see a PG-13 movie alone");
+} else if(age >= 5) {
+    document.getElementById("answers").innerHTML =
+      ("You can see a G or PG movie alone.");
+} else {
+    document.getElementById("answers").innerHTML =
+      ("Uh. your too young for most things.");
+}
+console.log("Thanks for verifying you're age!");
 }

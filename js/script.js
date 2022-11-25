b03b022716a7e0744198f179a6cc7d4ba774e587
Justin-Lavoie16 <Justin-Lavoie16@users.noreply.github.com>
2022-11-25 18:46:55 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,25 +4,23 @@ if (navigator.serviceWorker) {
   })
 }
 
-"use strict"
+;("use strict")
 
 function myButtonClicked() {
- 
-  const age = document.getElementById("age").value;
-  
-  if(age >= 17) {
-     document.getElementById("answers").innerHTML =
-      ("You can see an R movie alone.");
-    
-} else if(age >= 13) {
+  const age = document.getElementById("age").value
+
+  if (age >= 17) {
     document.getElementById("answers").innerHTML =
-      ("You can see a PG-13 movie alone");
-} else if(age >= 5) {
+      "You can see an R movie alone."
+  } else if (age >= 13) {
     document.getElementById("answers").innerHTML =
-      ("You can see a G or PG movie alone.");
-} else {
+      "You can see a PG-13 movie alone"
+  } else if (age >= 5) {
     document.getElementById("answers").innerHTML =
-      ("Uh. your too young for most things.");
-}
-console.log("Thanks for verifying you're age!");
+      "You can see a G or PG movie alone."
+  } else {
+    document.getElementById("answers").innerHTML =
+      "Uh. your too young for most things."
+  }
+  console.log("Thanks for verifying you're age!")
 }

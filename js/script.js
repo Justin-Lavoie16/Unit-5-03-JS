37f2fc5a62505570c85d4e74cca658ee473ce74f
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-12-01 14:00:31 -0500

    ewq

--- js/script.js
+++ js/script.js
@@ -12,15 +12,18 @@ function myButtonClicked() {
   if (age >= 17) {
     document.getElementById("answers").innerHTML =
       "You can see an R movie alone."
-  } else if (age >= 13) {
+  }
+  else if (age >= 13) {
     document.getElementById("answers").innerHTML =
       "You can see a PG-13 movie alone"
-  } else if (age >= 5) {
+  }
+  else if (age >= 5) {
     document.getElementById("answers").innerHTML =
       "You can see a G or PG movie alone."
-  } else {
+  }
+  else {
     document.getElementById("answers").innerHTML =
       "Uh. your too young for most things."
   }
   console.log("Thanks for verifying you're age!")
-}
+  }
